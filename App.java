9af43c983bbbf444b5c54799c998cf4ca006b4e6
VivekSingh883 <singhvivekviv789@gmail.com>
2026-04-12 20:16:21 +0530

    added deployment

--- App.java
+++ App.java
@@ -1,21 +1,5 @@
-import java.io.*;
-import java.net.*;
-
 public class App {
-    public static void main(String[] args) throws Exception {
-        ServerSocket server = new ServerSocket(3000);
-        System.out.println("Server started on port 3000");
-
-        while (true) {
-            Socket client = server.accept();
-            PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-
-            out.println("HTTP/1.1 200 OK");
-            out.println("Content-Type: text/plain");
-            out.println();
-            out.println("Hello DevOps Practical 1 🚀 (Java)");
-
-            client.close();
-        }
+    public static void main(String[] args) {
+        System.out.println("Hello DevOps Pipeline!");
     }
 }
